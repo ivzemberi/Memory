a7cd9a9885c6cffb730ee1efab086ecc8cff3e27
Ivana <ivana@zemberi.me>
2021-11-04 12:17:44 +0100

    Add responsiveness to cards by using protocol inheritance

--- Memory/EmojiMemoryGame.swift
+++ Memory/EmojiMemoryGame.swift
@@ -13,7 +13,7 @@ class EmojiMemoryGame: ObservableObject {
     private static let emojis = ["🚂", "🚜", "🚀", "✈️", "🚗", "🚙", "🚎", "🛻", "🚕", "🚘", "🚛", "🏍", "🚁", "⛵️", "🚐", "🛫", "🚲", "🚚", "🛩", "🚌", "🚃", "🚉", "🏎", "🛸"]
     
     private static func createMemoryGame() -> MemoryGame<String>{
-        MemoryGame<String>(numberOfPairsOfCards: 12) { pairIndex in
+        MemoryGame<String>(numberOfPairsOfCards: 6) { pairIndex in
             emojis[pairIndex]
         }
     }
